919b71766ad412f225f604b6169dd827f4da6cfa
Dickson Ronko <dicksonmanchau@gmail.com>
2024-01-18 09:09:23 -0800

    bug fixed

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,6 @@
-#!/usr/bin/pythoni3
-
+#!/usr/bin/python3
 def print_last_digit(number):
-    print(abs(number) % 10, end="")
-    return (abs(number) % 10)
+    if number < 0:
+        number *= -1
+    print("{}".format(number % 10), end="")
+    return number % 10
