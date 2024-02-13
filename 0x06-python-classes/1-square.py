45a67ca07e47a773337be103f5611edebf3485ee
Dickson Ronko <dicksonmanchau@gmail.com>
2024-02-13 05:41:27 -0800

    1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self,size):
+        """
+        Initializes a Square instance with a given size.
+
+        Args:
+            size (int): The size of the square (no type or value verification).
+        """
+        self.__size = size
