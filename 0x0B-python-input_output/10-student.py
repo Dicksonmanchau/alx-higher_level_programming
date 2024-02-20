e1ae288070698c0aa4290e1e34e640a268f66cd8
Dickson Ronko <dicksonmanchau@gmail.com>
2024-02-20 11:49:46 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""Student to JSON"""
 
 
 class Student:
-    """Represent a student."""
+    """Student class definition"""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+        """Constructor initializater"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
+        """Method to retrieves a dictionary representation
+        if attrs is a list of strings, only attribute names contained
+        in this lists must be retrieved, otherwise, all attributes
+        must be retrieved"""
 
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
+        result = {}
 
-        Args:
-            attrs (list): (Optional) The attributes to represent.
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+        if attrs is None:
+            return (self.__dict__)
+
+        for key in attrs:
+            """If not find key in the class return None, for that
+            this class should be transform in dictionary"""
+            value = self.__dict__.get(key)
+            if value is not None:
+                result[key] = value
+
+        return (result)
